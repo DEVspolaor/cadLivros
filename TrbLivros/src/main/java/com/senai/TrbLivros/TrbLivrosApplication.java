ee402bf7a41025da15332966814bf565421aa696
Spolaor <133894740+DEVspolaor@users.noreply.github.com>
2023-11-06 15:54:39 -0300

    Add files via upload

---
+++ TrbLivros/src/main/java/com/senai/TrbLivros/TrbLivrosApplication.java
@@ -0,0 +1,13 @@
+package com.senai.TrbLivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrbLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrbLivrosApplication.class, args);
+	}
+
+}
